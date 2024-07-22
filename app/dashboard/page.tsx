8a3351aa4c8e0ae2a5f6c3b36bf54afa49d04509
Josue Gonzalez <josuegr834@gmail.com>
2024-07-22 20:53:15 -0300

    Siguiendo con el curso de Next.js

--- app/dashboard/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Page() {
-  return (
-    <>
-      <h1 className="uppercase text-4xl font-semibold">Dashboard page</h1>
-    </>
-  );
-}
--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,20 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <link rel="icon" href="/favicon.svg" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          name="description"
+          content="Pagina de ejemplo para el curso de Next.js Learn. Simula un negocio de e-commerce, con una interfaz de usuario de alta calidad. "
+        />
+        <meta name="author" content="Josue Gonzalez" />
+        <title>
+          Acme - A comprehensive application designed to optimize the management
+          of your business.
+        </title>
+      </head>
       <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
